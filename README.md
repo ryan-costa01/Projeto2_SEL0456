6b59fbf4495eda26374ab6f97ae2c746f64010a9
Ryan Costa <63657064+ryan-costa01@users.noreply.github.com>
2023-10-13 23:11:11 +0000

    Add program and .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# Projeto2_SEL0456
+<a name="readme-top"></a>
+
+
+# Projeto2 SEL0456
 Segundo projeto da matéria SEL0456 - Técnicas em Desenvolvimento de Software Livre.
+
+## :wrench: Explicação do programa
+ Ao inicializar o programa, o usuário deverá inserir um número inteiro entre 1 e 20, onde esse número definirá o tamanho do vetor que será gerado.
+
+ Ex: `input: 5 -> vetor[5]`
+     `input: 8 -> vetor[8]`
+
+Com o tamanho definido, o programa gera um vetor aleatório e imprime no terminal os seguintes resultados:
+
+```
+Input:
+3
+Output:
+Vetor gerado aleatoriamente:
+x y z
+Vetor elevado ao quadrado:
+x^2 y^2 z^2
+A soma dos valores do vetor:
+ x+y+z
+A media dos valores do vetor:
+ x+y+z/3
+```
+Exemplo prático:
+```
+Input:
+3
+Output:
+Vetor gerado aleatoriamente:
+1.74 8.79 3.83
+Vetor elevado ao quadrado:
+3.01 77.24 14.70
+A soma dos valores do vetor:
+ 14.36
+A media dos valores do vetor:
+ 4.79
+```
+
+## :rocket: Rodando o projeto
+
+1. Clone o repositório
+   ```sh
+   git clone git@github.com:ryan-costa01/Projeto1_SEL0456.git
+   ```
+2. Compile o programa atraves do comando definido pelo `makefile`
+   ```cmd
+   make
+   ```
+3. Inicialize o programa
+   ```cmd
+   make run
+   ```
+4. Para apagar os arquivos `/*.o`
+   ```cmd
+   make clean
+   ```
+## :memo: License
+
+Distributed under the GNU General Public License v3.0. See `LICENSE.txt` for more information.
+
+## :handshake: Colaboradores
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/ryan-costa01">
+        <img src="https://avatars.githubusercontent.com/u/63657064?s=400&u=cae3d15c188ed977d1713fb373a5a42a145ae3ba&v=4" width="100px;" alt="Foto de Ryan Costa no GitHub"/><br>
+        <sub>
+          <b>Ryan Costa</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ proj_main.py
@@ -0,0 +1,154 @@
+"""
+Module providing a function printing python version.
+SEL0456 - Desenvolvimento de Software Livre
+Arquivo MAIN do Trabalho 2
+Author: Ryan Fellipe Silva Costa - ryanfellipe2001@usp.br
+n°USP: 11953369
+"""
+import hashlib
+import os
+import pickle
+
+# Definição da classe referente ao usuário
+class Usuario:
+    """"Definição da classe referente ao usuário"""
+    def __init__(self, nome, senha, hierarquia):
+        self.nome = nome
+        self.hierarquia = hierarquia
+        self.salt = os.urandom(16)  # Gerar um valor de "salt" aleatório
+        self.hash_senha = self._hash_senha(senha)
+
+    def _hash_senha(self, senha):
+        """Criar um hash a partir da senha e do 'salt'"""
+        senha_concatenada = senha.encode('utf-8') + self.salt
+        hash_obj = hashlib.sha256(senha_concatenada)
+        return hash_obj.hexdigest()
+
+    def verificar_senha(self, senha_inserida):
+        """Verificar se a senha inserida corresponde ao hash armazenado"""
+        senha_hash_inserida = self._hash_senha(senha_inserida)
+        return senha_hash_inserida == self.hash_senha
+    def __str__(self):
+        return self.__class__.__name__
+
+# Imprime os usuários salvos
+def __str__(self):
+    """Imprime os usuários salvos"""
+    print(f"Nome: {self.nome} Hierarquia: {self.hierarquia}")
+
+# Lista para armazenar usuários
+usuarios = []
+
+# Função para adicionar um novo usuário à lista
+def adicionar_usuario(nome, senha, hierarquia):
+    """Função para adicionar um novo usuário à lista"""
+    novo_usuario = Usuario(nome, senha, hierarquia)
+    usuarios.append(novo_usuario)
+
+# Função para verificar e retornar um usuário existente
+def obter_usuario_por_nome(nome):
+    """Função para verificar e retornar um usuário existente"""
+    for usuario in usuarios:
+        if usuario.nome == nome:
+            return usuario
+    return None
+
+# Função para validar hierarquia escolhida"""
+def select_hierarquia(nome_usuario, senha_inserida, op_hierarquia):
+    """Função para validar hierarquia escolhida"""    
+    while True:
+
+        if op_hierarquia in ('a', 'b', 'c'):
+
+            if op_hierarquia == 'a':
+                hierarquia = 'MasterAdmin'
+            elif op_hierarquia == 'b':
+                hierarquia = 'Admin'
+            elif op_hierarquia == 'c':
+                hierarquia = 'Padrão'
+
+            adicionar_usuario(nome_usuario, senha_inserida, hierarquia)
+
+            break
+        else:
+            print("Hierarquia inválida. ")
+            print("As hierarquias válidas são 'MasterAdmin', 'Admin' e 'Padrão'.")
+            op_hierarquia = input('Tente novamente:')
+
+# Função para analisar o nome de usuário
+def analisar_nome(nome):
+    """"Função para analisar o nome de usuário"""
+    while not nome.isalpha():
+        nome = input("Nome Inválido (Não utilize espaço nem caracteres especiais):")
+    return nome
+
+# Função para salvar usuários em um arquivo
+def salvar_usuarios():
+    """"Função para salvar usuários em um arquivo"""
+    with open('usuarios.pkl', 'wb') as arquivo:
+        pickle.dump(usuarios, arquivo)
+
+# Função para carregar usuários de um arquivo
+def carregar_usuarios():
+    """Função para carregar usuários de um arquivo"""
+    try:
+        with open('usuarios.pkl', 'rb') as arquivo:
+            return pickle.load(arquivo)
+    except FileNotFoundError:
+        return []
+
+# Carregar usuários do arquivo (se existir)
+usuarios = carregar_usuarios()
+
+# Menu de opções
+def menu():
+    """"Menu de opções"""
+    while True:
+        print("Opções:")
+        print("1. Adicionar novo usuário")
+        print("2. Usar usuário existente")
+        print("3. Usuários exixtentes")
+        print("4. Fechar Programa")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            nome = input("Digite o nome de usuário: ")
+            nome_usuario = analisar_nome(nome)
+            senha_inserida = input("Digite a senha: ")
+            op_hierarquia = input("Selecione a hierarquia:\na.MasterAdmin\nb.Admin\nc.Padrão\n\nDigite 'a','b' ou 'c': ")
+            select_hierarquia(nome_usuario, senha_inserida, op_hierarquia)
+            salvar_usuarios()
+            print("Usuários salvos com sucesso.")
+        elif opcao == "2":
+            while True:
+                nome = input("Digite o nome de usuário: ")
+                nome_usuario = analisar_nome(nome)
+                senha_inserida = input("Digite a senha: ")
+                usuario = obter_usuario_por_nome(nome_usuario)
+                if usuario and usuario.verificar_senha(senha_inserida):
+                    print("Senha correta para o usuário", usuario.nome)
+                    print("Hierarquia:", usuario.hierarquia)
+                    
+                    break
+                print("Senha incorreta ou usuário não encontrado.\n")
+                sim_nao = input("Quer tentar novamente?(s/n):")
+                if sim_nao == "n":
+                    break
+                
+        # Imprimir informações dos usuários
+        elif opcao == "3":
+            for usuario in usuarios:
+                __str__(usuario)
+        
+        # Fechar Programa
+        elif opcao == "4":
+            exit(0)
+        
+        # Mensagem de Erro em Caso de Entrada errada
+        else:
+            print("Opção inválida. Tente novamente.")
+
+# Exemplo de uso:
+menu()
+
+# End-of-file (EOF)
